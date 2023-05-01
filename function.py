eb0124bfdc544ecd7dd2ce2db13212f9b47f096f
chirchirmercy <cmercychptoo.com>
2023-05-01 23:57:41 +0300

    first commit

---
+++ function.py
@@ -0,0 +1,73 @@
+
+# Write a Python function that takes two arguments (a and b) and returns their sum.
+def sum_numbers(a,b):
+    sum=0
+    result=a+b
+    
+    return result
+print(sum_numbers(5,7))    
+
+
+# Write a Python function that takes a string as input and returns the string reversed.
+
+def reverse_String(word):
+    return word[::-1]
+word2=reverse_String("mercy")
+print(word2)
+
+        
+
+# Write a Python function that takes a list of integers as input and returns the sum of all the integers in the list.
+ def integer(*nums):
+        sum=0
+    for x in integer:
+        sum+=x
+        return sum
+print(integers(2,4,5))
+        
+# Write a Python function that takes a list of integers as input and returns a new list with all the even numbers removed.
+def even_numbers(numbers):
+    new=[]
+        for x in numbers:
+        if x%2!=0
+        new.append(x):
+        return new
+        print(even_numbers(4,5,7,8,3,9))
+
+    
+
+# Write a Python function that takes a list of integers as input and returns the highest value in the list.
+def highest_value(num):
+    new=max(num)
+    return new
+print(highest_value([20,10,7,9,5]))
+
+
+ 
+
+
+
+
+# Write a Python function that takes a list of strings as input and returns a new list with all the strings capitalized
+
+def Convert_tittle(names):
+    name=names.upper():
+    return name
+print(Convert_tittle("mercy"))
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+        
